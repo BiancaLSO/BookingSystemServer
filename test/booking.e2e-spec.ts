b9b592a96a608ef08a6dd4610d2292573c672c22
Bianca <biancastan90@gmail.com>
2023-02-14 09:28:06 +0100

    Test GET bookings

--- test/booking.e2e-spec.ts
+++ test/booking.e2e-spec.ts
@@ -3,35 +3,100 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { BookingDto } from './../src/bookings/entities/booking.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Booking } from './../src/bookings/entities/booking.entity';
+import { Connection, Repository } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let moduleFixture: TestingModule;
+  let bookingRepo: Repository<Booking>;
+  let connection: Connection;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    bookingRepo = moduleFixture.get(getRepositoryToken(Booking));
+
+    connection = moduleFixture.get(Connection);
+
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('should create a new valid booking (POST)', async () => {
-    const booking = new BookingDto(
-      'Minnie',
-      5,
-      new Date(),
-      '1234567',
-      'minnie@yahoo.com',
-      'Allergic to garlic',
-    );
+  afterEach(async () => {
+    await connection.dropDatabase();
+    await connection.runMigrations();
+    await moduleFixture.close();
+  });
 
-    const { body } = await request(app.getHttpServer())
-      .post('/booking')
-      .send(booking)
-      .expect(201);
+  describe('GET Bookings', () => {
+    it('should retrieve all bookings', async () => {
+      await Promise.all([
+        bookingRepo.insert(
+          new BookingDto(
+            'Lisa',
+            2,
+            new Date(2023, 0, 15),
+            '1234567',
+            'lisa@gmail.com',
+            'Allergic to nuts',
+          ),
+        ),
+        bookingRepo.insert(
+          new BookingDto(
+            'Jisoo',
+            4,
+            new Date(2023, 2, 15),
+            '1234567',
+            'jisoo@gmail.com',
+            'Table by the window',
+          ),
+        ),
+        bookingRepo.insert(
+          new BookingDto(
+            'Rose',
+            4,
+            new Date(2023, 3, 20),
+            '1234567',
+            'rose@gmail.com',
+            'One vegan',
+          ),
+        ),
+      ]);
 
-    expect(body.name).toEqual('Minnie');
+      // Act
+      const { body }: { body: Booking[] } = await request(app.getHttpServer())
+        .get('/booking')
+        .expect(200);
+
+      // Assert
+      expect(body.length).toEqual(3);
+    });
+  });
+
+  describe('POST a Booking', () => {
+    it('should create a new valid booking', async () => {
+      const booking = new BookingDto(
+        'Minnie',
+        5,
+        new Date(),
+        '1234567',
+        'minnie@yahoo.com',
+        'Allergic to garlic',
+      );
+
+      // Act
+      const { body } = await request(app.getHttpServer())
+        .post('/booking')
+        .send(booking)
+        .expect(201);
+
+      // Assert
+      expect(body.name).toEqual('Minnie');
+    });
   });
 
   afterAll(() => {
